6d038301d74daa86b9afdd31df6b48db68438247
Alfonso Antognozzi <al.antognozzicaraffa@studenti.unicam.it>
2023-07-05 15:28:36 +0200

    Third gui Release

--- app/src/main/java/it/unicam/cs/AlfonsoAntognozzi/App/SceneGameController.java
+++ app/src/main/java/it/unicam/cs/AlfonsoAntognozzi/App/SceneGameController.java
@@ -1,18 +1,19 @@
 package it.unicam.cs.AlfonsoAntognozzi.App;
 
-import it.unicam.cs.AlfonsoAntognozzi.model.Environment;
-import it.unicam.cs.AlfonsoAntognozzi.model.Handler;
-import it.unicam.cs.AlfonsoAntognozzi.model.IRobot;
-import it.unicam.cs.AlfonsoAntognozzi.model.Robot;
+import it.unicam.cs.AlfonsoAntognozzi.io.ShapeCreator;
+import it.unicam.cs.AlfonsoAntognozzi.model.*;
 import it.unicam.cs.AlfonsoAntognozzi.util.FollowMeParser;
 import it.unicam.cs.AlfonsoAntognozzi.util.FollowMeParserException;
 import it.unicam.cs.AlfonsoAntognozzi.util.Position;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -27,7 +28,9 @@ import java.util.Random;
 public class SceneGameController {
 
     @FXML
-    AnchorPane gameMap;
+    AnchorPane generalMap;
+    @FXML
+    Pane gameMap;
     @FXML
     Button zoomIn;
     @FXML
@@ -40,6 +43,11 @@ public class SceneGameController {
     Button stepBack;
     @FXML
     Button stepForw;
+    @FXML
+    ToolBar ButtonMenu;
+    private static final double ZOOM_FACTOR = 0.1;
+    private double zoomLevel;
+    private ShapeCreator SC;
     final Stage commandChooserStage = new Stage();
     final Stage shapeChooserStage = new Stage();
     private Environment env;
@@ -49,21 +57,22 @@ public class SceneGameController {
 
 
     public void initilizeRobot(int numberOfRobot){
+        zoomLevel=gameMap.getScaleX();
         Random random = new Random();
         for(int i = 0; i<numberOfRobot;i++){
-            double xPosition = random.nextDouble(1000);
-            double yPosition = random.nextDouble(900);
+            double xPosition = random.nextDouble(999);
+            double yPosition = random.nextDouble(763);
             robotList.add(new Robot(new Position(xPosition,yPosition)));
             Image image = new Image("robot che si muove.jpg");
             ImageView imageView = new ImageView(image);
             imageView.setFitWidth(15);
             imageView.setFitHeight(15);
-            AnchorPane.setTopAnchor(imageView,yPosition);
-            AnchorPane.setLeftAnchor(imageView,xPosition);
             gameMap.getChildren().add(imageView);
         }
+        System.out.println(gameMap.getChildren().size());
     env = new Environment(robotList);
     gameParser = new FollowMeParser(new Handler(env));
+    SC = new ShapeCreator<>(env);
     }
     public void addCommandToGame(javafx.event.ActionEvent actionEvent) throws Exception {
         try {
@@ -85,29 +94,58 @@ public class SceneGameController {
             String desktopPath = view.getHomeDirectory().getPath();
             fileChooser.setInitialDirectory(new File(desktopPath));
             File selectedFile = fileChooser.showOpenDialog(shapeChooserStage);
-            gameParser.parseEnvironment(selectedFile);
+            SC.parseShape(selectedFile);
+            generateSelectedShapes();
         }catch (NullPointerException e ){ System.out.println("Non hai selezionato un file!");}
     }
+
+    private void generateSelectedShapes() {
+        for (int i = 0; i < env.shapeList().size(); i++) {
+            IShape S = env.shapeList().get(i);
+            if (S instanceof Rectangle) {
+                javafx.scene.shape.Rectangle R = new javafx.scene.shape.Rectangle(S.getShapePosition().getX(), S.getShapePosition().getY(),
+                        ((Rectangle) S).getWidth(), ((Rectangle) S).getHeight());
+                R.setFill(Color.TRANSPARENT);
+                R.setStroke(Color.BLACK);
+                gameMap.getChildren().add(R);
+                System.out.println("Questo è un rettangolo");
+            } else if (S instanceof Circle) {
+                javafx.scene.shape.Circle C = new javafx.scene.shape.Circle(S.getShapePosition().getX(), S.getShapePosition().getY(), ((Circle) S).getRadius());
+                C.setFill(Color.TRANSPARENT);
+                C.setStroke(Color.BLACK);
+                gameMap.getChildren().add(C);
+                System.out.println("Questo è un cerchio");
+            }
+        }
+    }
+
 
     public void stepForwCommand(javafx.event.ActionEvent actionEvent) {
         if(this.env.hasNextInstruction()) {
             this.env.executeNextIstruction();
-            for(int i = 1; i <= env.robotList().size(); i++){
+            for(int i = 0; i < env.robotList().size(); i++){
                 TranslateTransition translate = new TranslateTransition();
                 translate.setDuration(Duration.millis(1000));
                 translate.setNode(gameMap.getChildren().get(i));
-                translate.setByX(env.robotList().get(i-1).getRobotPosition().getX()-gameMap.getChildren().get(i).getLayoutX());
-                translate.setByY(env.robotList().get(i-1).getRobotPosition().getY()-gameMap.getChildren().get(i).getLayoutY());
+                translate.setByX(env.robotList().get(i).getRobotPosition().getX()- gameMap.getChildren().get(i).getLayoutX());
+                translate.setByY(env.robotList().get(i).getRobotPosition().getY()- gameMap.getChildren().get(i).getLayoutY());
                 translate.play();
             }
         }
     }
 
     public void zoomInEvent(javafx.event.ActionEvent actionEvent){
+        zoomLevel+=ZOOM_FACTOR;
+        gameMap.setScaleY(zoomLevel);
+        gameMap.setScaleX(zoomLevel);
 
     }
 
     public void zoomOutEvent(javafx.event.ActionEvent actionEvent){
+        zoomLevel-=ZOOM_FACTOR;
+        if(zoomLevel<0) zoomLevel=0;
+        gameMap.setScaleY(zoomLevel);
+        gameMap.setScaleX(zoomLevel);
 
     }
 
